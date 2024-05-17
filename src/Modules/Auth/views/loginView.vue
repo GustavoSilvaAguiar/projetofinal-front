c59beee75c5ce998c55594cd08ac57166fbf0848
Gustavo Silva <gustavogo109@gmail.com>
2024-05-17 11:44:36 -0300

    Config de axios

---
+++ src/Modules/Auth/views/loginView.vue
@@ -0,0 +1,21 @@
+<template><div>das hfghdf dasd</div></template>
+
+<script setup lang="ts">
+import apiCore from '@/services/api'
+import { onMounted } from 'vue'
+
+const makelogin = async () => {
+  const response = await apiCore.post('login', {
+    login: 'gustavo.teste@teste.com',
+    senha: '987.456.184-88',
+    device_name: 'Front teste'
+  })
+  return response.data.token
+}
+
+onMounted(async () => {
+  console.log(await makelogin())
+})
+</script>
+
+<style scoped lang="scss"></style>
